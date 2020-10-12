8fe73d9b906658b505490a11f0dff1b3306d0df2
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-12 18:59:44 +0300

    Add bound 'Vector.__delitem__' magic method

--- src/main.cpp
+++ src/main.cpp
@@ -184,6 +184,19 @@ static BackwardIterator<Collection> to_backward_iterator(
   return {collection};
 }
 
+template <class Collection>
+static void delete_item(Collection& collection, Index index) {
+  Index size = to_size(collection);
+  Index normalized_index = index >= 0 ? index : index + size;
+  if (normalized_index < 0 || normalized_index >= size)
+    throw std::out_of_range(size ? (std::string("Index should be in range(" +
+                                                std::to_string(-size) + ", ") +
+                                    std::to_string(size) + "), but found " +
+                                    std::to_string(index) + ".")
+                                 : std::string("Sequence is empty."));
+  collection.erase(std::next(std::begin(collection), normalized_index));
+}
+
 template <class Sequence>
 static const typename Sequence::value_type& to_item(const Sequence& sequence,
                                                     Index index) {
@@ -246,6 +259,50 @@ PYBIND11_MODULE(MODULE_NAME, m) {
           result.push_back(py::reinterpret_borrow<Object>(element));
         return result;
       }))
+      .def("__delitem__", delete_item<Vector>, py::arg("index"))
+      .def(
+          "__delitem__",
+          [](Vector& self, py::slice slice) {
+            auto size = to_size(self);
+            std::size_t raw_start, raw_stop, raw_step, slice_length;
+            if (!slice.compute(size, &raw_start, &raw_stop, &raw_step,
+                               &slice_length))
+              throw py::error_already_set();
+            auto start = static_cast<Index>(raw_start);
+            auto stop = static_cast<Index>(raw_stop);
+            auto step = static_cast<Index>(raw_step);
+            if (step == 1)
+              self.erase(std::next(self.begin(), start),
+                         std::next(self.begin(), stop));
+            else if (step == -1)
+              self.erase(std::next(self.begin(), stop + 1),
+                         std::next(self.begin(), start + 1));
+            else if (step > 0) {
+              const auto& begin = self.begin();
+              Vector result{begin, std::next(begin, start)};
+              result.reserve(size - slice_length);
+              for (; step < stop - start; start += step)
+                result.insert(result.end(), std::next(begin, start + 1),
+                              std::next(begin, start + step));
+              result.insert(result.end(), std::next(begin, start + 1),
+                            self.end());
+              self.assign(result.begin(), result.end());
+            } else {
+              start = size - start - 1;
+              stop = size - stop - 1;
+              step = -step;
+              const auto& rbegin = self.rbegin();
+              Vector result{rbegin, std::next(rbegin, start)};
+              result.reserve(size - slice_length);
+              for (; step < stop - start; start += step)
+                result.insert(result.end(), std::next(rbegin, start + 1),
+                              std::next(rbegin, start + step));
+              result.insert(result.end(), std::next(rbegin, start + 1),
+                            self.rend());
+              self.assign(result.rbegin(), result.rend());
+            }
+          },
+          py::arg("slice"))
       .def("__getitem__", to_item<Vector>, py::arg("index"))
       .def(
           "__getitem__",
