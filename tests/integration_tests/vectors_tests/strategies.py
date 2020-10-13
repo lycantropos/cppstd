1b4504634dc039feebcba609a16cb476fbb5500d
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-13 22:27:06 +0300

    Add test for bound & ported 'Vector.__getitem__' magic methods

--- tests/integration_tests/vectors_tests/strategies.py
+++ tests/integration_tests/vectors_tests/strategies.py
@@ -1,6 +1,10 @@
+from typing import Tuple
+
 from hypothesis import strategies
 
-from tests.utils import to_bound_ported_vectors_pair
+from tests.utils import (BoundPortedVectorsPair,
+                         Strategy,
+                         to_bound_ported_vectors_pair)
 
 objects = strategies.integers()
 empty_lists = strategies.builds(list)
@@ -13,3 +17,28 @@ empty_vectors_pairs = strategies.builds(to_bound_ported_vectors_pair,
                                         empty_lists)
 non_empty_vectors_pairs = strategies.builds(to_bound_ported_vectors_pair,
                                             non_empty_objects_lists)
+
+
+def to_non_empty_vectors_pairs_with_indices(
+        pair: BoundPortedVectorsPair
+) -> Strategy[Tuple[BoundPortedVectorsPair, int]]:
+    bound, _ = pair
+    size = len(bound)
+    return strategies.tuples(strategies.just(pair),
+                             strategies.integers(-size, size - 1))
+
+
+non_empty_vectors_pairs_with_indices = (
+    non_empty_vectors_pairs.flatmap(to_non_empty_vectors_pairs_with_indices))
+
+
+def to_non_empty_vectors_pairs_with_slices(
+        pair: BoundPortedVectorsPair
+) -> Strategy[Tuple[BoundPortedVectorsPair, slice]]:
+    bound, _ = pair
+    size = len(bound)
+    return strategies.tuples(strategies.just(pair), strategies.slices(size))
+
+
+non_empty_vectors_pairs_with_slices = (
+    non_empty_vectors_pairs.flatmap(to_non_empty_vectors_pairs_with_slices))
---
+++ tests/integration_tests/vectors_tests/test_getitem.py
@@ -0,0 +1,25 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from tests.utils import (BoundPortedVectorsPair,
+                         are_bound_ported_vectors_equal)
+from . import strategies
+
+
+@given(strategies.non_empty_vectors_pairs_with_indices)
+def test_index(pair_with_index: Tuple[BoundPortedVectorsPair, int]) -> None:
+    (bound, ported), index = pair_with_index
+
+    bound_result, ported_result = bound[index], ported[index]
+
+    assert bound_result == ported_result
+
+
+@given(strategies.non_empty_vectors_pairs_with_slices)
+def test_slice(pair_with_index: Tuple[BoundPortedVectorsPair, int]) -> None:
+    (bound, ported), slice_ = pair_with_index
+
+    bound_result, ported_result = bound[slice_], ported[slice_]
+
+    assert are_bound_ported_vectors_equal(bound_result, ported_result)
