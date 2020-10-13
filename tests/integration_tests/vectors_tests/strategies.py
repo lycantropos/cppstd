76fcd70e38ced0b34c2ea5e70f5e275bc105f9e5
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-13 22:20:50 +0300

    Add test for bound & ported 'Vector.pop' methods

--- tests/integration_tests/vectors_tests/strategies.py
+++ tests/integration_tests/vectors_tests/strategies.py
@@ -3,6 +3,13 @@ from hypothesis import strategies
 from tests.utils import to_bound_ported_vectors_pair
 
 objects = strategies.integers()
+empty_lists = strategies.builds(list)
 objects_lists = strategies.lists(objects)
+non_empty_objects_lists = strategies.lists(objects,
+                                           min_size=1)
 vectors_pairs = strategies.builds(to_bound_ported_vectors_pair,
                                   objects_lists)
+empty_vectors_pairs = strategies.builds(to_bound_ported_vectors_pair,
+                                        empty_lists)
+non_empty_vectors_pairs = strategies.builds(to_bound_ported_vectors_pair,
+                                            non_empty_objects_lists)
