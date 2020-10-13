abf4064bc1ad09c61947a4c896ec25af2e5d592a
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-13 16:57:24 +0300

    Complete ported 'vector' module

--- cppstd/vector.py
+++ cppstd/vector.py
@@ -1,5 +1,6 @@
 from typing import (Generic,
                     Iterable,
+                    Iterator,
                     List,
                     Union,
                     overload)
@@ -38,6 +39,9 @@ class Vector(Generic[Domain]):
     def __getitem__(self, item):
         return self._values[item]
 
+    def __iter__(self) -> 'VectorForwardIterator[Domain]':
+        return VectorForwardIterator(0, self)
+
     def __len__(self) -> int:
         return len(self._values)
 
@@ -55,7 +59,61 @@ class Vector(Generic[Domain]):
     def append(self, value: Domain) -> None:
         self._values.append(value)
 
+    def begin(self) -> 'VectorForwardIterator[Domain]':
+        return VectorForwardIterator(0, self)
+
+    def end(self) -> 'VectorForwardIterator[Domain]':
+        return VectorForwardIterator(len(self), self)
+
     def insert(self, index: int, value: Domain) -> None:
         self._values.insert(index, value)
 
     push_back = append
+
+
+class VectorForwardIterator(Iterator[Domain]):
+    __slots__ = '_index', '_vector'
+
+    def __init__(self, index: int, vector: Vector) -> None:
+        self._index = index
+        self._vector = vector
+
+    def __add__(self, offset: int) -> 'VectorForwardIterator[Domain]':
+        return VectorForwardIterator(self._move_index(offset), self._vector)
+
+    def __iadd__(self, offset: int) -> 'VectorForwardIterator[Domain]':
+        self._index = self._move_index(offset)
+        return self
+
+    def __isub__(self, offset: int) -> 'VectorForwardIterator[Domain]':
+        self._index = self._move_index(-offset)
+        return self
+
+    def __iter__(self) -> 'VectorForwardIterator[Domain]':
+        return self
+
+    def __next__(self) -> Domain:
+        try:
+            result = self._vector[self._index]
+        except IndexError:
+            raise StopIteration from None
+        else:
+            self._index += 1
+            return result
+
+    def __sub__(self, offset: int) -> 'VectorForwardIterator[Domain]':
+        return VectorForwardIterator(self._move_index(-offset), self._vector)
+
+    def _move_index(self, offset: int) -> int:
+        size = len(self._vector)
+        min_offset, max_offset = -self._index, size - self._index
+        if offset < min_offset or offset > max_offset:
+            raise ValueError('Offset should be '
+                             'in range({min_offset}, {max_offset}), '
+                             'but found {offset}.'
+                             .format(min_offset=min_offset,
+                                     max_offset=max_offset + 1,
+                                     offset=offset)
+                             if size
+                             else 'Vector is empty.')
+        return self._index + offset
