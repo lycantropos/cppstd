19f7b9793eb20c310cec3fb0dac4631b83685273
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-18 16:48:55 +0300

    Add ported iterators invalidation

---
+++ cppstd/core/tokenization.py
@@ -0,0 +1,31 @@
+import weakref
+from typing import Callable, Protocol
+
+
+class WeakReferencable(Protocol):
+    __weakref__ = ...
+
+
+class Token:
+    __slots__ = '_value',
+
+    def __init__(self, value: WeakReferencable) -> None:
+        self._value = weakref.ref(value)
+
+    @property
+    def expired(self) -> bool:
+        return self._value() is None
+
+
+class Tokenizer:
+    _value_factory = set  # type: Callable[[], WeakReferencable]
+
+    def __init__(self) -> None:
+        self._value = self._value_factory()
+
+    def create(self) -> Token:
+        return Token(self._value)
+
+    def reset(self) -> None:
+        del self._value
+        self._value = self._value_factory()
--- cppstd/vector.py
+++ cppstd/vector.py
@@ -11,15 +11,18 @@ from typing import (Generic,
 from reprit.base import (generate_repr,
                          seekers)
 
+from .core.tokenization import (Token,
+                                Tokenizer)
 from .hints import Domain
 
 
 @abc.MutableSequence.register
 class Vector(Generic[Domain]):
-    __slots__ = '_values',
+    __slots__ = '_values', '_tokenizer'
 
     def __init__(self, *values: Domain) -> None:
         self._values = list(values)  # type: List[Domain]
+        self._tokenizer = Tokenizer()
 
     __repr__ = generate_repr(__init__,
                              field_seeker=seekers.complex_)
@@ -52,7 +55,7 @@ class Vector(Generic[Domain]):
                 else Vector(*self._values[item]))
 
     def __iter__(self) -> 'VectorForwardIterator[Domain]':
-        return VectorForwardIterator(0, self)
+        return VectorForwardIterator(0, self._values, self._tokenizer.create())
 
     def __len__(self) -> int:
         return len(self._values)
@@ -68,7 +71,8 @@ class Vector(Generic[Domain]):
                 else NotImplemented)
 
     def __reversed__(self) -> 'VectorBackwardIterator[Domain]':
-        return VectorBackwardIterator(0, self)
+        return VectorBackwardIterator(0, self._values,
+                                      self._tokenizer.create())
 
     @overload
     def __setitem__(self, item: int, value: Domain) -> None:
@@ -79,25 +83,42 @@ class Vector(Generic[Domain]):
         """Sets subvector by given slice to given values."""
 
     def __setitem__(self, item, value):
+        if isinstance(item, slice):
+            start, stop, step = item.indices(len(self._values))
+            value = list(value)
+            if stop > start or step == 1 and value:
+                self._tokenizer.reset()
         self._values[item] = value
 
     def append(self, value: Domain) -> None:
+        self._tokenizer.reset()
         self._values.append(value)
 
     def begin(self) -> 'VectorForwardIterator[Domain]':
-        return VectorForwardIterator(0, self)
+        return VectorForwardIterator(0, self._values,
+                                     self._tokenizer.create())
 
     def clear(self) -> None:
+        self._tokenizer.reset()
         self._values.clear()
 
     def count(self, value: Domain) -> int:
         return self._values.count(value)
 
     def end(self) -> 'VectorForwardIterator[Domain]':
-        return VectorForwardIterator(len(self), self)
+        return VectorForwardIterator(len(self), self._values,
+                                     self._tokenizer.create())
 
     def extend(self, values: Iterable[Domain]) -> None:
-        self._values.extend(values)
+        iterator = iter(values)
+        try:
+            value = next(iterator)
+        except StopIteration:
+            return
+        else:
+            self._tokenizer.reset()
+        self._values.append(value)
+        self._values.extend(iterator)
 
     def index(self,
               value: Domain,
@@ -106,45 +127,67 @@ class Vector(Generic[Domain]):
         return self._values.index(value, start, stop)
 
     def insert(self, index: int, value: Domain) -> None:
+        self._tokenizer.reset()
         self._values.insert(index, value)
 
     def pop(self, index: int = -1) -> None:
+        if self._values:
+            self._tokenizer.reset()
         return self._values.pop(index)
 
     def pop_back(self) -> None:
+        self._tokenizer.reset()
         del self._values[-1]
 
     push_back = append
 
     def rbegin(self) -> 'VectorBackwardIterator[Domain]':
-        return VectorBackwardIterator(0, self)
+        return VectorBackwardIterator(0, self._values,
+                                      self._tokenizer.create())
 
     def remove(self, value: Domain) -> None:
-        self._values.remove(value)
+        try:
+            index = self._values.index(value)
+        except ValueError:
+            raise
+        else:
+            self._tokenizer.reset()
+            del self._values[index]
 
     def rend(self) -> 'VectorBackwardIterator[Domain]':
-        return VectorBackwardIterator(len(self), self)
+        return VectorBackwardIterator(len(self), self._values,
+                                      self._tokenizer.create())
 
     def resize(self, size: int, value: Optional[Domain] = None) -> None:
         if size < 0:
             raise ValueError('Size should be positive, but found {}.'
                              .format(size))
+        self._tokenizer.reset()
         self._values = (self._values
                         + [value] * (size - len(self._values)))[:size]
 
     def reverse(self) -> None:
+        self._tokenizer.reset()
         self._values.reverse()
 
 
 class VectorBackwardIterator(Iterator[Domain]):
-    __slots__ = '_index', '_vector'
+    __slots__ = '_index', '_values', '_token'
 
-    def __init__(self, index: int, vector: Vector) -> None:
+    def __init__(self, index: int, values: List[Domain], token: Token) -> None:
         self._index = index
-        self._vector = vector
+        self._values = values
+        self._token = token
 
     def __add__(self, offset: int) -> 'VectorBackwardIterator[Domain]':
-        return VectorBackwardIterator(self._move_index(offset), self._vector)
+        return VectorBackwardIterator(self._move_index(offset), self._values,
+                                      self._token)
+
+    def __eq__(self, other: 'VectorBackwardIterator[Domain]') -> bool:
+        return (self._to_validated_values() is other._to_validated_values()
+                and self._index == other._index
+                if isinstance(other, VectorBackwardIterator)
+                else NotImplemented)
 
     def __iadd__(self, offset: int) -> 'VectorBackwardIterator[Domain]':
         self._index = self._move_index(offset)
@@ -158,18 +201,21 @@ class VectorBackwardIterator(Iterator[Domain]):
         return self
 
     def __le__(self, other: 'VectorBackwardIterator[Domain]') -> bool:
-        return (self._vector is other._vector and self._index <= other._index
+        return (self._to_validated_values() is other._to_validated_values()
+                and self._index <= other._index
                 if isinstance(other, VectorBackwardIterator)
                 else NotImplemented)
 
     def __lt__(self, other: 'VectorBackwardIterator[Domain]') -> bool:
-        return (self._vector is other._vector and self._index < other._index
+        return (self._to_validated_values() is other._to_validated_values()
+                and self._index < other._index
                 if isinstance(other, VectorBackwardIterator)
                 else NotImplemented)
 
     def __next__(self) -> Domain:
+        self._validate()
         try:
-            result = self._vector[-self._index - 1]
+            result = self._values[-self._index - 1]
         except IndexError:
             raise StopIteration from None
         else:
@@ -177,10 +223,12 @@ class VectorBackwardIterator(Iterator[Domain]):
             return result
 
     def __sub__(self, offset: int) -> 'VectorBackwardIterator[Domain]':
-        return VectorBackwardIterator(self._move_index(-offset), self._vector)
+        return VectorBackwardIterator(self._move_index(-offset), self._values,
+                                      self._token)
 
     def _move_index(self, offset: int) -> int:
-        size = len(self._vector)
+        self._validate()
+        size = len(self._values)
         min_offset, max_offset = -self._index, size - self._index
         if offset < min_offset or offset > max_offset:
             raise ValueError('Offset should be '
@@ -193,16 +241,32 @@ class VectorBackwardIterator(Iterator[Domain]):
                              else 'Vector is empty.')
         return self._index + offset
 
+    def _to_validated_values(self) -> List[Domain]:
+        self._validate()
+        return self._values
+
+    def _validate(self) -> None:
+        if self._token.expired:
+            raise ValueError('Iterator is invalidated.')
+
 
 class VectorForwardIterator(Iterator[Domain]):
-    __slots__ = '_index', '_vector'
+    __slots__ = '_index', '_values', '_token'
 
-    def __init__(self, index: int, vector: Vector) -> None:
+    def __init__(self, index: int, values: List[Domain], token: Token) -> None:
         self._index = index
-        self._vector = vector
+        self._values = values
+        self._token = token
 
     def __add__(self, offset: int) -> 'VectorForwardIterator[Domain]':
-        return VectorForwardIterator(self._move_index(offset), self._vector)
+        return VectorForwardIterator(self._move_index(offset), self._values,
+                                     self._token)
+
+    def __eq__(self, other: 'VectorForwardIterator[Domain]') -> bool:
+        return (self._to_validated_values() is other._to_validated_values()
+                and self._index == other._index
+                if isinstance(other, VectorForwardIterator)
+                else NotImplemented)
 
     def __iadd__(self, offset: int) -> 'VectorForwardIterator[Domain]':
         self._index = self._move_index(offset)
@@ -216,18 +280,21 @@ class VectorForwardIterator(Iterator[Domain]):
         return self
 
     def __le__(self, other: 'VectorForwardIterator[Domain]') -> bool:
-        return (self._vector is other._vector and self._index <= other._index
+        return (self._to_validated_values() is other._to_validated_values()
+                and self._index <= other._index
                 if isinstance(other, VectorForwardIterator)
                 else NotImplemented)
 
     def __lt__(self, other: 'VectorForwardIterator[Domain]') -> bool:
-        return (self._vector is other._vector and self._index < other._index
+        return (self._to_validated_values() is other._to_validated_values()
+                and self._index < other._index
                 if isinstance(other, VectorForwardIterator)
                 else NotImplemented)
 
     def __next__(self) -> Domain:
+        self._validate()
         try:
-            result = self._vector[self._index]
+            result = self._values[self._index]
         except IndexError:
             raise StopIteration from None
         else:
@@ -235,10 +302,12 @@ class VectorForwardIterator(Iterator[Domain]):
             return result
 
     def __sub__(self, offset: int) -> 'VectorForwardIterator[Domain]':
-        return VectorForwardIterator(self._move_index(-offset), self._vector)
+        return VectorForwardIterator(self._move_index(-offset), self._values,
+                                     self._token)
 
     def _move_index(self, offset: int) -> int:
-        size = len(self._vector)
+        self._validate()
+        size = len(self._values)
         min_offset, max_offset = -self._index, size - self._index
         if offset < min_offset or offset > max_offset:
             raise ValueError('Offset should be '
@@ -250,3 +319,11 @@ class VectorForwardIterator(Iterator[Domain]):
                              if size
                              else 'Vector is empty.')
         return self._index + offset
+
+    def _to_validated_values(self) -> List[Domain]:
+        self._validate()
+        return self._values
+
+    def _validate(self) -> None:
+        if self._token.expired:
+            raise ValueError('Iterator is invalidated.')
