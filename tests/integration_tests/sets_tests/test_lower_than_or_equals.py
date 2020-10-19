a22a668c780ec70f5c6f40da85161e3f977e1796
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-19 18:59:05 +0300

    Add test for bound & ported 'Set.__le__' magic methods

---
+++ tests/integration_tests/sets_tests/test_lower_than_or_equals.py
@@ -0,0 +1,15 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedSetsPair,
+                         equivalence)
+from . import strategies
+
+
+@given(strategies.sets_pairs, strategies.sets_pairs)
+def test_basic(first_pair: BoundPortedSetsPair,
+               second_pair: BoundPortedSetsPair) -> None:
+    first_bound, first_ported = first_pair
+    second_bound, second_ported = second_pair
+
+    assert equivalence(first_bound <= second_bound,
+                       first_ported <= second_ported)
