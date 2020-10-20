58822eb91237d2fac4ecdf8d2d347d7d52bdd6de
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-20 20:27:01 +0300

    Add test for bound & ported 'Map.__bool__' magic methods

---
+++ tests/integration_tests/maps_tests/test_bool.py
@@ -0,0 +1,11 @@
+from hypothesis import given
+
+from tests.utils import BoundPortedMapsPair
+from . import strategies
+
+
+@given(strategies.maps_pairs)
+def test_basic(pair: BoundPortedMapsPair) -> None:
+    bound, ported = pair
+
+    assert bool(bound) is bool(ported)
