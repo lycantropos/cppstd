434c085f79d8822e895bf598266439433f15c9fb
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-20 16:46:28 +0300

    Complete ported 'red_black' module

--- cppstd/core/red_black.py
+++ cppstd/core/red_black.py
@@ -1,6 +1,7 @@
+from abc import (ABC,
+                 abstractmethod)
 from itertools import islice
-from typing import (Iterator,
-                    Tuple,
+from typing import (Tuple,
                     Union)
 
 from dendroid.red_black import (NIL,
@@ -21,7 +22,7 @@ def index_to_node(index: int, tree: Tree) -> Node:
     return next(islice(tree, index, None))
 
 
-class TreeBackwardIterator(Iterator[Node]):
+class TreeIterator(ABC):
     __slots__ = '_index', '_node', '_tree', '_token'
 
     def __init__(self,
@@ -34,26 +35,63 @@ class TreeBackwardIterator(Iterator[Node]):
         self._tree = tree
         self._token = token
 
-    def __add__(self, offset: int) -> 'TreeBackwardIterator[Node]':
-        return TreeBackwardIterator(*self._move_node(offset), self._tree,
-                                    self._token)
+    def __add__(self, offset: int) -> 'TreeIterator[Node]':
+        return type(self)(*self._move_node(offset), self._tree, self._token)
 
-    def __eq__(self, other: 'TreeBackwardIterator[Node]') -> bool:
+    def __eq__(self, other: 'TreeIterator[Node]') -> bool:
         return (self._to_validated_node() is other._to_validated_node()
-                if isinstance(other, TreeBackwardIterator)
+                if isinstance(other, type(self))
                 else NotImplemented)
 
-    def __iadd__(self, offset: int) -> 'TreeBackwardIterator[Node]':
+    def __iadd__(self, offset: int) -> 'TreeIterator[Node]':
         self._index, self._node = self._move_node(offset)
         return self
 
-    def __isub__(self, offset: int) -> 'TreeBackwardIterator[Node]':
+    def __isub__(self, offset: int) -> 'TreeIterator[Node]':
         self._index, self._node = self._move_node(-offset)
         return self
 
-    def __iter__(self) -> 'TreeBackwardIterator[Node]':
+    def __iter__(self) -> 'TreeIterator[Node]':
         return self
 
+    @abstractmethod
+    def __next__(self) -> Node:
+        """Returns current node while moving to the next one."""
+
+    def __sub__(self, offset: int) -> 'TreeIterator[Node]':
+        return type(self)(*self._move_node(-offset), self._tree, self._token)
+
+    @abstractmethod
+    def _index_to_node(self, index: int, size: int) -> AnyNode:
+        """Returns node by given index."""
+
+    def _move_node(self, offset: int) -> Tuple[int, AnyNode]:
+        self._validate()
+        index = self._index
+        size = len(self._tree)
+        min_offset, max_offset = -index, size - index
+        if offset < min_offset or offset > max_offset:
+            raise ValueError('Offset should be '
+                             'in range({min_offset}, {max_offset}), '
+                             'but found {offset}.'
+                             .format(min_offset=min_offset,
+                                     max_offset=max_offset + 1,
+                                     offset=offset)
+                             if size
+                             else 'Container is empty.')
+        new_index = index + offset
+        return new_index, self._index_to_node(new_index, size)
+
+    def _to_validated_node(self) -> AnyNode:
+        self._validate()
+        return self._node
+
+    def _validate(self) -> None:
+        if self._token.expired:
+            raise ValueError('Iterator is invalidated.')
+
+
+class TreeBackwardIterator(TreeIterator):
     def __next__(self) -> Node:
         result = self._to_validated_node()
         if result is NIL:
@@ -62,71 +100,13 @@ class TreeBackwardIterator(Iterator[Node]):
         self._node = self._tree.predecessor(self._node)
         return result
 
-    def __sub__(self, offset: int) -> 'TreeBackwardIterator[Node]':
-        return TreeBackwardIterator(*self._move_node(-offset), self._tree,
-                                    self._token)
-
-    def _move_node(self, offset: int) -> Tuple[int, AnyNode]:
-        self._validate()
-        index = self._index
-        size = len(self._tree)
-        min_offset, max_offset = -index, size - index
-        if offset < min_offset or offset > max_offset:
-            raise ValueError('Offset should be '
-                             'in range({min_offset}, {max_offset}), '
-                             'but found {offset}.'
-                             .format(min_offset=min_offset,
-                                     max_offset=max_offset + 1,
-                                     offset=offset)
-                             if size
-                             else 'Container is empty.')
-        new_index = index + offset
-        return new_index, (index_to_node(size - new_index - 1, self._tree)
-                           if new_index < size
-                           else NIL)
-
-    def _to_validated_node(self) -> AnyNode:
-        self._validate()
-        return self._node
-
-    def _validate(self) -> None:
-        if self._token.expired:
-            raise ValueError('Iterator is invalidated.')
+    def _index_to_node(self, index: int, size: int) -> AnyNode:
+        return (index_to_node(size - index - 1, self._tree)
+                if index < size
+                else NIL)
 
 
-class TreeForwardIterator(Iterator[Node]):
-    __slots__ = '_index', '_node', '_tree', '_token'
-
-    def __init__(self,
-                 index: int,
-                 node: AnyNode,
-                 tree: Tree[Node, Node],
-                 token: Token) -> None:
-        self._index = index
-        self._node = node
-        self._tree = tree
-        self._token = token
-
-    def __add__(self, offset: int) -> 'TreeForwardIterator[Node]':
-        return TreeForwardIterator(*self._move_node(offset), self._tree,
-                                   self._token)
-
-    def __eq__(self, other: 'TreeForwardIterator[Node]') -> bool:
-        return (self._to_validated_node() is other._to_validated_node()
-                if isinstance(other, TreeForwardIterator)
-                else NotImplemented)
-
-    def __iadd__(self, offset: int) -> 'TreeForwardIterator[Node]':
-        self._index, self._node = self._move_node(offset)
-        return self
-
-    def __isub__(self, offset: int) -> 'TreeForwardIterator[Node]':
-        self._index, self._node = self._move_node(-offset)
-        return self
-
-    def __iter__(self) -> 'TreeForwardIterator[Node]':
-        return self
-
+class TreeForwardIterator(TreeIterator):
     def __next__(self) -> Node:
         result = self._to_validated_node()
         if result is NIL:
@@ -135,33 +115,7 @@ class TreeForwardIterator(Iterator[Node]):
         self._node = self._tree.successor(self._node)
         return result
 
-    def __sub__(self, offset: int) -> 'TreeForwardIterator[Node]':
-        return TreeForwardIterator(*self._move_node(-offset), self._tree,
-                                   self._token)
-
-    def _move_node(self, offset: int) -> Tuple[int, AnyNode]:
-        self._validate()
-        index = self._index
-        size = len(self._tree)
-        min_offset, max_offset = -index, size - index
-        if offset < min_offset or offset > max_offset:
-            raise ValueError('Offset should be '
-                             'in range({min_offset}, {max_offset}), '
-                             'but found {offset}.'
-                             .format(min_offset=min_offset,
-                                     max_offset=max_offset + 1,
-                                     offset=offset)
-                             if size
-                             else 'Container is empty.')
-        new_index = index + offset
-        return new_index, (index_to_node(new_index, self._tree)
-                           if new_index < size
-                           else NIL)
-
-    def _to_validated_node(self) -> AnyNode:
-        self._validate()
-        return self._node
-
-    def _validate(self) -> None:
-        if self._token.expired:
-            raise ValueError('Iterator is invalidated.')
+    def _index_to_node(self, index: int, size: int) -> AnyNode:
+        return (index_to_node(index, self._tree)
+                if index < size
+                else NIL)
