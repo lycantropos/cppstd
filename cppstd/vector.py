449aa1ad65b96974bbd8b48a71e33c07a2797b4a
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-22 22:47:38 +0300

    Complete ported 'vector' class

--- cppstd/vector.py
+++ cppstd/vector.py
@@ -13,6 +13,136 @@ from .hints import Value
 
 
 class vector(Generic[Value]):
+    class iterator(Generic[Value]):
+        __slots__ = '_index', '_values', '_token'
+
+        def __init__(self, index: int, values: List[Value],
+                     token: Token) -> None:
+            self._index = index
+            self._values = values
+            self._token = token
+
+        def __add__(self, offset: int) -> 'vector.iterator[Value]':
+            return vector.iterator(self._move_index(offset), self._values,
+                                   self._token)
+
+        def __eq__(self, other: 'vector.iterator[Value]') -> bool:
+            return (self._to_validated_values() is other._to_validated_values()
+                    and self._index == other._index
+                    if isinstance(other, vector.iterator)
+                    else NotImplemented)
+
+        def __iadd__(self, offset: int) -> 'vector.iterator[Value]':
+            self._index = self._move_index(offset)
+            return self
+
+        def __isub__(self, offset: int) -> 'vector.iterator[Value]':
+            self._index = self._move_index(-offset)
+            return self
+
+        def __le__(self, other: 'vector.iterator[Value]') -> bool:
+            return (self._to_validated_values() is other._to_validated_values()
+                    and self._index <= other._index
+                    if isinstance(other, vector.iterator)
+                    else NotImplemented)
+
+        def __lt__(self, other: 'vector.iterator[Value]') -> bool:
+            return (self._to_validated_values() is other._to_validated_values()
+                    and self._index < other._index
+                    if isinstance(other, vector.iterator)
+                    else NotImplemented)
+
+        def __sub__(self, offset: int) -> 'vector.iterator[Value]':
+            return vector.iterator(self._move_index(-offset), self._values,
+                                   self._token)
+
+        def _move_index(self, offset: int) -> int:
+            size = len(self._to_validated_values())
+            min_offset, max_offset = -self._index, size - self._index
+            if offset < min_offset or offset > max_offset:
+                raise ValueError('Offset should be '
+                                 'in range({min_offset}, {max_offset}), '
+                                 'but found {offset}.'
+                                 .format(min_offset=min_offset,
+                                         max_offset=max_offset + 1,
+                                         offset=offset)
+                                 if size
+                                 else 'Vector is empty.')
+            return self._index + offset
+
+        def _to_validated_values(self) -> List[Value]:
+            self._validate()
+            return self._values
+
+        def _validate(self) -> None:
+            if self._token.expired:
+                raise ValueError('Iterator is invalidated.')
+
+    class reverse_iterator(Generic[Value]):
+        __slots__ = '_index', '_values', '_token'
+
+        def __init__(self, index: int, values: List[Value],
+                     token: Token) -> None:
+            self._index = index
+            self._values = values
+            self._token = token
+
+        def __add__(self, offset: int) -> 'vector.reverse_iterator[Value]':
+            return vector.reverse_iterator(self._move_index(offset),
+                                           self._values, self._token)
+
+        def __eq__(self, other: 'vector.reverse_iterator[Value]') -> bool:
+            return (self._to_validated_values() is other._to_validated_values()
+                    and self._index == other._index
+                    if isinstance(other, vector.reverse_iterator)
+                    else NotImplemented)
+
+        def __iadd__(self, offset: int) -> 'vector.reverse_iterator[Value]':
+            self._index = self._move_index(offset)
+            return self
+
+        def __isub__(self, offset: int) -> 'vector.reverse_iterator[Value]':
+            self._index = self._move_index(-offset)
+            return self
+
+        def __le__(self, other: 'vector.reverse_iterator[Value]') -> bool:
+            return (self._to_validated_values() is other._to_validated_values()
+                    and self._index <= other._index
+                    if isinstance(other, vector.reverse_iterator)
+                    else NotImplemented)
+
+        def __lt__(self, other: 'vector.reverse_iterator[Value]') -> bool:
+            return (self._to_validated_values() is other._to_validated_values()
+                    and self._index < other._index
+                    if isinstance(other, vector.reverse_iterator)
+                    else NotImplemented)
+
+        def __sub__(self, offset: int) -> 'vector.reverse_iterator[Value]':
+            return vector.reverse_iterator(self._move_index(-offset),
+                                           self._values, self._token)
+
+        def _move_index(self, offset: int) -> int:
+            size = len(self._to_validated_values())
+            min_offset, max_offset = -self._index, size - self._index
+            if offset < min_offset or offset > max_offset:
+                raise ValueError('Offset should be '
+                                 'in range({min_offset}, {max_offset}), '
+                                 'but found {offset}.'
+                                 .format(min_offset=min_offset,
+                                         max_offset=max_offset + 1,
+                                         offset=offset)
+                                 if size
+                                 else 'Vector is empty.')
+            return self._index + offset
+
+        def _to_validated_values(self) -> List[Value]:
+            self._validate()
+            return self._values
+
+        def _validate(self) -> None:
+            if self._token.expired:
+                raise ValueError('Iterator is invalidated.')
+
     __slots__ = '_values', '_tokenizer'
 
     def __init__(self, *values: Value) -> None:
@@ -60,17 +190,16 @@ class vector(Generic[Value]):
                 self._tokenizer.reset()
         self._values[item] = value
 
-    def begin(self) -> 'VectorForwardIterator[Value]':
-        return VectorForwardIterator(0, self._values,
-                                     self._tokenizer.create())
+    def begin(self) -> 'iterator[Value]':
+        return vector.iterator(0, self._values, self._tokenizer.create())
 
     def clear(self) -> None:
         self._tokenizer.reset()
         self._values.clear()
 
-    def end(self) -> 'VectorForwardIterator[Value]':
-        return VectorForwardIterator(self.size(), self._values,
-                                     self._tokenizer.create())
+    def end(self) -> 'iterator[Value]':
+        return vector.iterator(self.size(), self._values,
+                               self._tokenizer.create())
 
     def insert(self, index: int, value: Value) -> None:
         self._tokenizer.reset()
@@ -90,13 +219,13 @@ class vector(Generic[Value]):
         self._tokenizer.reset()
         self._values.append(value)
 
-    def rbegin(self) -> 'VectorBackwardIterator[Value]':
-        return VectorBackwardIterator(0, self._values,
-                                      self._tokenizer.create())
+    def rbegin(self) -> 'reverse_iterator[Value]':
+        return vector.reverse_iterator(0, self._values,
+                                       self._tokenizer.create())
 
-    def rend(self) -> 'VectorBackwardIterator[Value]':
-        return VectorBackwardIterator(self.size(), self._values,
-                                      self._tokenizer.create())
+    def rend(self) -> 'reverse_iterator[Value]':
+        return vector.reverse_iterator(self.size(), self._values,
+                                       self._tokenizer.create())
 
     def resize(self, size: int, value: Optional[Value] = None) -> None:
         if size < 0:
@@ -105,133 +234,3 @@ class vector(Generic[Value]):
         self._tokenizer.reset()
         self._values = (self._values
                         + [value] * (size - len(self._values)))[:size]
-
-
-class VectorBackwardIterator(Generic[Value]):
-    __slots__ = '_index', '_values', '_token'
-
-    def __init__(self, index: int, values: List[Value], token: Token) -> None:
-        self._index = index
-        self._values = values
-        self._token = token
-
-    def __add__(self, offset: int) -> 'VectorBackwardIterator[Value]':
-        return VectorBackwardIterator(self._move_index(offset), self._values,
-                                      self._token)
-
-    def __eq__(self, other: 'VectorBackwardIterator[Value]') -> bool:
-        return (self._to_validated_values() is other._to_validated_values()
-                and self._index == other._index
-                if isinstance(other, VectorBackwardIterator)
-                else NotImplemented)
-
-    def __iadd__(self, offset: int) -> 'VectorBackwardIterator[Value]':
-        self._index = self._move_index(offset)
-        return self
-
-    def __isub__(self, offset: int) -> 'VectorBackwardIterator[Value]':
-        self._index = self._move_index(-offset)
-        return self
-
-    def __le__(self, other: 'VectorBackwardIterator[Value]') -> bool:
-        return (self._to_validated_values() is other._to_validated_values()
-                and self._index <= other._index
-                if isinstance(other, VectorBackwardIterator)
-                else NotImplemented)
-
-    def __lt__(self, other: 'VectorBackwardIterator[Value]') -> bool:
-        return (self._to_validated_values() is other._to_validated_values()
-                and self._index < other._index
-                if isinstance(other, VectorBackwardIterator)
-                else NotImplemented)
-
-    def __sub__(self, offset: int) -> 'VectorBackwardIterator[Value]':
-        return VectorBackwardIterator(self._move_index(-offset), self._values,
-                                      self._token)
-
-    def _move_index(self, offset: int) -> int:
-        size = len(self._to_validated_values())
-        min_offset, max_offset = -self._index, size - self._index
-        if offset < min_offset or offset > max_offset:
-            raise ValueError('Offset should be '
-                             'in range({min_offset}, {max_offset}), '
-                             'but found {offset}.'
-                             .format(min_offset=min_offset,
-                                     max_offset=max_offset + 1,
-                                     offset=offset)
-                             if size
-                             else 'Vector is empty.')
-        return self._index + offset
-
-    def _to_validated_values(self) -> List[Value]:
-        self._validate()
-        return self._values
-
-    def _validate(self) -> None:
-        if self._token.expired:
-            raise ValueError('Iterator is invalidated.')
-
-
-class VectorForwardIterator(Generic[Value]):
-    __slots__ = '_index', '_values', '_token'
-
-    def __init__(self, index: int, values: List[Value], token: Token) -> None:
-        self._index = index
-        self._values = values
-        self._token = token
-
-    def __add__(self, offset: int) -> 'VectorForwardIterator[Value]':
-        return VectorForwardIterator(self._move_index(offset), self._values,
-                                     self._token)
-
-    def __eq__(self, other: 'VectorForwardIterator[Value]') -> bool:
-        return (self._to_validated_values() is other._to_validated_values()
-                and self._index == other._index
-                if isinstance(other, VectorForwardIterator)
-                else NotImplemented)
-
-    def __iadd__(self, offset: int) -> 'VectorForwardIterator[Value]':
-        self._index = self._move_index(offset)
-        return self
-
-    def __isub__(self, offset: int) -> 'VectorForwardIterator[Value]':
-        self._index = self._move_index(-offset)
-        return self
-
-    def __le__(self, other: 'VectorForwardIterator[Value]') -> bool:
-        return (self._to_validated_values() is other._to_validated_values()
-                and self._index <= other._index
-                if isinstance(other, VectorForwardIterator)
-                else NotImplemented)
-
-    def __lt__(self, other: 'VectorForwardIterator[Value]') -> bool:
-        return (self._to_validated_values() is other._to_validated_values()
-                and self._index < other._index
-                if isinstance(other, VectorForwardIterator)
-                else NotImplemented)
-
-    def __sub__(self, offset: int) -> 'VectorForwardIterator[Value]':
-        return VectorForwardIterator(self._move_index(-offset), self._values,
-                                     self._token)
-
-    def _move_index(self, offset: int) -> int:
-        size = len(self._to_validated_values())
-        min_offset, max_offset = -self._index, size - self._index
-        if offset < min_offset or offset > max_offset:
-            raise ValueError('Offset should be '
-                             'in range({min_offset}, {max_offset}), '
-                             'but found {offset}.'
-                             .format(min_offset=min_offset,
-                                     max_offset=max_offset + 1,
-                                     offset=offset)
-                             if size
-                             else 'Vector is empty.')
-        return self._index + offset
-
-    def _to_validated_values(self) -> List[Value]:
-        self._validate()
-        return self._values
-
-    def _validate(self) -> None:
-        if self._token.expired:
-            raise ValueError('Iterator is invalidated.')
