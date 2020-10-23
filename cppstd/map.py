5e5c8457bfd7f1b6ede87c6c6607360a9bb8fc88
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-23 18:01:41 +0300

    Complete ported 'map' class

--- cppstd/map.py
+++ cppstd/map.py
@@ -10,8 +10,27 @@ from .hints import (Item,
 
 
 class map(Generic[Key, Value]):
-    class iterator(red_black.TreeIterator, Generic[Key, Value]):
-        pass
+    class const_iterator(red_black.TreeIterator, Generic[Item]):
+        @property
+        def value(self) -> Item:
+            node = self._to_validated_node()
+            if node is red_black.NIL:
+                raise RuntimeError('Getting value of placeholder iterators '
+                                   'is undefined.')
+            return node.item
+
+    class const_reverse_iterator(red_black.TreeReverseIterator,
+                                 Generic[Item]):
+        @property
+        def value(self) -> Item:
+            node = self._to_validated_node()
+            if node is red_black.NIL:
+                raise RuntimeError('Getting value of placeholder iterators '
+                                   'is undefined.')
+            return node.item
+
+    iterator = const_iterator
+    reverse_iterator = const_reverse_iterator
 
     __slots__ = '_items', '_tokenizer'
 
@@ -34,16 +53,49 @@ class map(Generic[Key, Value]):
         else:
             node.value = value
 
-    def begin(self) -> iterator[Key, Value]:
+    def begin(self) -> iterator[Item]:
         return self.iterator(0, self._items.tree.min(), self._items.tree,
                              self._tokenizer.create_weak())
+
+    def cbegin(self) -> const_iterator[Item]:
+        return self.iterator(0, self._items.tree.min(), self._items.tree,
+                             self._tokenizer.create_weak())
+
+    def cend(self) -> const_iterator[Item]:
+        return self.const_iterator(len(self._items), red_black.NIL,
+                                   self._items.tree,
+                                   self._tokenizer.create_weak())
 
     def clear(self) -> None:
         self._tokenizer.reset()
         self._items.clear()
 
+    def crbegin(self) -> const_reverse_iterator[Item]:
+        return self.const_reverse_iterator(0, self._items.tree.max(),
+                                           self._items.tree,
+                                           self._tokenizer.create_weak())
+
+    def crend(self) -> const_reverse_iterator[Item]:
+        return self.const_reverse_iterator(len(self._items), red_black.NIL,
+                                           self._items.tree,
+                                           self._tokenizer.create_weak())
+
     def empty(self) -> bool:
         return not self._items
+
+    def end(self) -> iterator[Item]:
+        return self.iterator(len(self._items), red_black.NIL, self._items.tree,
+                             self._tokenizer.create_weak())
+
+    def rbegin(self) -> reverse_iterator[Item]:
+        return self.reverse_iterator(0, self._items.tree.max(),
+                                     self._items.tree,
+                                     self._tokenizer.create_weak())
+
+    def rend(self) -> reverse_iterator[Item]:
+        return self.reverse_iterator(len(self._items), red_black.NIL,
+                                     self._items.tree,
+                                     self._tokenizer.create_weak())
 
     def size(self) -> int:
         return len(self._items)
