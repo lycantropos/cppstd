81a6406cb8a1380871e11fc7a04c128e2bbdb7ea
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-24 00:27:10 +0300

    Add test for bound & ported 'map.rbegin' methods

---
+++ tests/integration_tests/maps_tests/test_rbegin.py
@@ -0,0 +1,15 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedMapsPair,
+                         are_bound_ported_map_iterators_equal)
+from . import strategies
+
+
+@given(strategies.maps_pairs)
+def test_basic(pair: BoundPortedMapsPair) -> None:
+    bound, ported = pair
+
+    bound_result, ported_result = bound.rbegin(), ported.rbegin()
+
+    assert are_bound_ported_map_iterators_equal(bound_result, bound.rend(),
+                                                ported_result, ported.rend())
